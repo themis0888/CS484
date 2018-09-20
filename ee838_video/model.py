0d82bc2428b77d370ebc81743b9b725d7b391b29
themis0888 <themis0888@gmail.com>
2018-09-20 23:15:00 +0900

    SISR model implemented

--- ee838_video/model.py
+++ ee838_video/model.py
@@ -38,12 +38,14 @@ class SISR:
 			'conv3':256,
 			'conv4':3
 		}
+		self.ratio = config.ratio
 		self.im_size = [self.height, self.width, self.channels]
 		self.n = 5
 		self.reuse = False
 
 		self.X = tf.placeholder(tf.float32, [None, self.width, self.height, self.channels])
-		self.Y = tf.placeholder(tf.float32, [None, self.n_classes])
+		self.Y = tf.placeholder(tf.float32, 
+		[None, self.ratio*self.width, self.ratio*self.height, self.channels])
 		
 		input_data = self.X
 		input_data = tf.layers.conv2d(inputs=input_data, filters=64,
@@ -66,8 +68,23 @@ class SISR:
 		# -------------------- Objective -------------------- #
 
 
+		self.cost = tf.losses.absolute_difference(self.Y, self.output_data)
+		total_var = tf.global_variables() 
+		self.optimizer = tf.train.AdamOptimizer(0.001, epsilon=0.01).minimize(self.cost)
+		
 		init = tf.global_variables_initializer()
 		self.sess.run(init)
+		self.saver = tf.train.Saver(total_var)
+
+		tf.train.start_queue_runners(sess=self.sess)
+
+	def train(self, x_data, y_data, training=True):
+		return self.sess.run([self.optimizer, self.cost], 
+			feed_dict={self.X: x_data, self.Y: y_data, self.training: training})
+
+	def test(self, x_test, training=False):
+		return self.sess.run(self.output_data, 
+			feed_dict={self.X: x_test, self.training: training})
 
 
 	def residual_block(self, input_layer, num_filter, kernel):
@@ -77,30 +94,6 @@ class SISR:
 					kernel_size = [7,7], padding="same")
 		return conv_layer + input_layer
 
-	def pixel_shuffle_layer(self, x, r, n_split):
-		def PS(x, r):
-			bs, a, b, c = x.get_shape().as_list()
-			x = tf.reshape(x, (bs, a, b, r, r))
-			x = tf.transpose(x, (0, 1, 2, 4, 3))
-			x = tf.split(x, a, 1)
-			x = tf.concat([tf.squeeze(x_) for x_ in x], 2)
-			x = tf.split(x, b, 1)
-			x = tf.concat([tf.squeeze(x_) for x_ in x], 2)
-			return tf.reshape(x, (bs, a*r, b*r, 1))
-
-		xc = tf.split(x, n_split, 3)
-		return tf.concat([tf.depth_to_space(x_,2) for x_ in xc], 3)
-		
-
-
-	def train(self, x_data, y_data, training=True):
-		return self.sess.run([self.optimizer, self.cost, self.merged, self.accuracy], feed_dict={self.X: x_data, self.Y: y_data, self.training: training})
-
-	def get_accuracy(self, x_test, y_test, training=False):
-		return self.sess.run(self.accuracy, feed_dict={self.X: x_test, self.Y: y_test, self.training: training})
-
-	def predict(self, x_data, training=False):
-		return self.sess.run(tf.argmax(self.logits, 1), feed_dict={self.X: x_data, self.training: training})
-
+	
 
 
--- ee838_video/training.py
+++ ee838_video/training.py
@@ -10,14 +10,15 @@ import tensorflow as tf
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_path', type=str, dest='data_path', default='/home/siit/navi/data/input_data/mnist_png/')
-parser.add_argument('--meta_path', type=str, dest='meta_path', default='/home/siit/navi/data/meta_data/mnist_png/')
+parser.add_argument('--data_path', type=str, dest='data_path', default='/home/siit/navi/data/input_data/mscoco/')
+parser.add_argument('--meta_path', type=str, dest='meta_path', default='/home/siit/navi/data/meta_data/mscoco/')
 parser.add_argument('--model_path', type=str, dest='model_path', default='/shared/data/models/')
 parser.add_argument('--epoch', type=int, dest='epoch', default=1000)
 
 parser.add_argument('--n_classes', type=int, dest='n_classes', default=10)
-parser.add_argument('--im_size', type=int, dest='im_size', default=28)
-parser.add_argument('--batch_size', type=int, dest='batch_size', default=100)
+parser.add_argument('--im_size', type=int, dest='im_size', default=64)
+parser.add_argument('--ratio', type=int, dest='ratio', default=2)
+parser.add_argument('--batch_size', type=int, dest='batch_size', default=16)
 
 parser.add_argument('--label_processed', type=bool, dest='label_processed', default=True)
 parser.add_argument('--save_freq', type=int, dest='save_freq', default=1000)
@@ -36,7 +37,7 @@ import os, random
 import data_loader
 import numpy as np
 import pdb
-
+from model import *
 
 # -------------------- Model -------------------- #
 depth = 3
@@ -45,21 +46,17 @@ height = config.im_size
 width = config.im_size
 channels = 3
 im_size = [height, width, channels]
+target_size = [config.ratio * height, config.ratio * width, channels]
 
-# model = __import__('201_CNN_model').CNN_model(sess, config, 'CNN_model')
-# model = __import__('202_VGG_model').Vgg19(sess, config, 'VGG_19')
-# model = __import__('203_ResNet_model').ResNet(sess, config, 'ResNet')
-model = __import__('204_ResNet_v2_model').ResNet_v2(sess, config, 'ResNet')
+model = SISR(sess, config, 'SISR')
 
 # -------------------- Data maniging -------------------- #
 
-#label_file = np.load(os.path.join('/shared/data/celeb_cartoon/','attributes.npz'))
-label_file = np.load(os.path.join(config.meta_path, 'path_label_dict.npy'))
+train_LR_files = [os.path.join(dp, f)
+		for dp, dn, filenames in os.walk(config.data_path)
+		for f in filenames if 'train/LR' in dp]
 
-list_files = [os.path.join(dp, f)
-		for dp, dn, filenames in os.walk(config.meta_path) 
-		for f in filenames if 'dict.npy' in f]
-list_files.sort()
+# list_files.sort()
 
 batch_size = config.batch_size
 
@@ -71,69 +68,48 @@ batch_size = config.batch_size
 
 counter = 0
 for epoch in range(config.epoch):
-	for list_file in list_files:
 
-		f = np.load(list_file)
+	random.shuffle(train_LR_files)
+
+	num_file = len(train_LR_files)
+
+	if num_file ==0:
+		break
+
+	total_batch = int(num_file / batch_size)
+	total_cost = 0
+	final_acc = 0
+
+	for i in range(total_batch):
+		# Get the batch as [batch_size, 28,28] and [batch_size, n_classes] ndarray
+
+		Xbatch = data_loader.queue_data_dict(
+			train_LR_files[i*batch_size:(i+1)*batch_size], im_size, config.label_processed)
+
+		train_HR_files = [os.path.join(config.data_path, 'train/HR', os.path.basename(file_path))
+			for file_path in train_LR_files[i*batch_size:(i+1)*batch_size]]
 		
-		path_label_dict = f.item()
-		input_file_list = list(path_label_dict.keys())
-		# You should shuffle the list. 
-		# The network will be stupid if you don't
-		random.shuffle(input_file_list)
-		all_data = [line for line in input_file_list]
-		train_data = all_data[1000:]
-		test_data = all_data[:1000]
+		Ybatch = data_loader.queue_data_dict(
+			train_HR_files, target_size, config.label_processed)
 
-		num_file = len(train_data)
+		_, cost_val = model.train(Xbatch, Ybatch)
 
-		if num_file ==0:
-			break
+		total_cost += cost_val
 
-		# print('Number of input files: \t{}'.format(num_file))
-		total_batch = int(num_file / batch_size)
-		total_cost = 0
-		final_acc = 0
+		counter += 1
 
-		for i in range(total_batch):
-			# Get the batch as [batch_size, 28,28] and [batch_size, n_classes] ndarray
-			label_list = np.expand_dims(path_label_dict[train_data[i*batch_size]], axis = 0)
-			for j in range(1, batch_size):
-				label_list = np.concatenate((label_list, np.expand_dims(
-					path_label_dict[train_data[i*batch_size + j]], 
-					axis = 0)), axis = 0)
-			Ybatch = np.reshape(label_list, [batch_size, config.n_classes])
+		if np.mod(counter, config.print_freq) == 0:
+			print('Step:', '%05dk' % (counter),
+				'\tAvg. cost =', '{:.5f}'.format(cost_val))
 
-			Xbatch = data_loader.queue_data_dict(
-				train_data[i*batch_size:(i+1)*batch_size], im_size, config.label_processed)
-
-			_, cost_val, acc, acc_ = model.sess.run(
-				[model.optimizer, model.cost, model.merged, model.accuracy], 
-				feed_dict={model.X: Xbatch, model.Y: Ybatch, model.training: True})
-
-			total_cost += cost_val
-
-			counter += 1
-
-			if np.mod(counter, config.print_freq) == 0:
-				print('Step:', '%05dk' % (counter),
-					'\tAvg. cost =', '{:.5f}'.format(cost_val),
-					'\tAcc: {:.5f}'.format(acc_))
-				model.writer.add_summary(acc, counter)
-
-			# Save the model
-			if np.mod(counter, config.save_freq) == 0:
-				if not os.path.exists(config.checkpoint_path):
-					os.mkdir(config.checkpoint_path)
-				model.saver.save(sess, os.path.join(config.checkpoint_path, 
-					'vgg19_{0:03d}k'.format(int(counter/1000))))
-				print('Model ')
+		# Save the model
+		if np.mod(counter, config.save_freq) == 0:
+			if not os.path.exists(config.checkpoint_path):
+				os.mkdir(config.checkpoint_path)
+			model.saver.save(sess, os.path.join(config.checkpoint_path, 
+				'vgg19_{0:03d}k'.format(int(counter/1000))))
+			print('Model ')
 	
 
 # -------------------- Testing -------------------- #
 
-
-Xbatch, Ybatch, _ = data_loader.queue_data(
-	test_data, label_list, im_size)
-
-accuracy_ = sess.run(accuracy, feed_dict = {X: Xbatch, Y: Ybatch})
-print('Accuracy:', accuracy_)
